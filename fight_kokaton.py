695f667572609570a66393050c229191298176bc
HEKUCHAN <heitorhirose@gmail.com>
2025-04-29 14:27:48 +0900

    delete useless comment

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -87,31 +87,6 @@ class Bird:
         screen.blit(self.img, self.rct)
 
 
-# ビームクラス:
-# """
-# こうかとんが放つビームに関するクラス
-# """
-# def イニシャライザ(self, bird:"Bird"):
-#     """
-#     ビーム画像Surfaceを生成する
-#     引数 bird：ビームを放つこうかとん（Birdインスタンス）
-#     """
-#     self.img = pg.画像のロード(f"fig/beam.png")
-#     self.rct = self.img.Rectの取得()
-#     self.ビームの中心縦座標 = こうかとんの中心縦座標
-#     self.ビームの左座標 = こうかとんの右座標
-#     self.vx, self.vy = +5, 0
-
-# def update(self, screen: pg.Surface):
-#     """
-#     ビームを速度ベクトルself.vx, self.vyに基づき移動させる
-#     引数 screen：画面Surface
-#     """
-#     if check_bound(self.rct) == (True, True):
-#         self.rct.move_ip(self.vx, self.vy)
-#         screen.blit(self.img, self.rct)
-
-
 class Beam:
     """
     こうかとんが放つビームに関するクラス
@@ -175,9 +150,7 @@ def main():
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("fig/pg_bg.jpg")
     bird = Bird((300, 200))
-    bombs: list[Bomb | None] = [
-        Bomb((255, 0, 0), 10) for _ in range(NUM_OF_BOMBS)
-    ]
+    bombs: list[Bomb | None] = [Bomb((255, 0, 0), 10) for _ in range(NUM_OF_BOMBS)]
     clock = pg.time.Clock()
     tmr = 0
     beam = None
@@ -208,7 +181,7 @@ def main():
                             return
                     font = pg.font.Font(None, 80)
                     txt = font.render("Game Over", True, (255, 0, 0))
-                    screen.blit(txt, [WIDTH//2-150, HEIGHT//2])
+                    screen.blit(txt, [WIDTH // 2 - 150, HEIGHT // 2])
                     pg.display.update()
                     game_over_clock.tick(50)
 
